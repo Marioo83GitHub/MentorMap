61f5e4695b07931aa5f724f15e66f10be4a80760
Marioo83GitHub <mcarbajalg@unah.hn>
2025-09-11 20:11:49 -0600

    agregado usuarios sencillos de mentor y estudiante para pruebas

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Mentor;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 
-class UserSeeder extends Seeder
-{
+class UserSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(): void {
         $user = User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@mm.com',
@@ -41,6 +40,10 @@ class UserSeeder extends Seeder
             'password' => bcrypt('123'),
         ]);
         $user->assignRole(Role::create(['name' => 'mentor']));
+        // create student
+        Mentor::create([
+            'user_id' => $user->id,
+        ]);
 
         $user = User::factory()->create([
             'email' => 'mario@gmail.com',
