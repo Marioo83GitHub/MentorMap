63b489cdb90e1d3d34343f24b9e5d3c257a3d089
acxel.aplicano <acxel.aplicano@unah.hn>
2025-09-16 13:53:39 -0600

    arreglo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/Login/style.css', 'resources/js/Login/script.js'],
             refresh: true,
         }),
         tailwindcss(),
